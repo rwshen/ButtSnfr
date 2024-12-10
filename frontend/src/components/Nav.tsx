0e257c8a2f4dcb20bbfaf4b64ece21f4f8032437
rwshen <rshen91@gmail.com>
2024-12-10 14:15:16 -0700

    some tailwind css

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link'
 export const Nav = () => {
   return (
     <nav
-      className='flex-no-wrap relative flex w-full items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4'
+      className='flex-no-wrap relative flex w-full items-center justify-between bg-blue py-2 shadow-dark-mild dark:bg-neutral-700 lg:flex-wrap lg:justify-start lg:py-4'
     >
       <div className='flex w-full flex-wrap items-center justify-between px-3'>
         <Link
-          className='block border-0 bg-transparent px-2 text-black/50 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden'
+          className='hover:text-white block border-0 bg-transparent px-2 text-black/50 hover:no-underline hover:shadow-none focus:no-underline focus:shadow-none focus:outline-none focus:ring-0 dark:text-neutral-200 lg:hidden'
           href='/'
         >
           ButtSnfr
         </Link>
 
         <div className='relative flex items-center'>
-          <Link className='me-4 text-neutral-600 dark:text-white' href='/messages'>
+          <Link className='hover:text-white me-4 text-neutral-600 dark:text-white' href='/messages'>
             <span className='[&>svg]:w-5'>
               Bark Center
             </span>
@@ -26,14 +26,13 @@ export const Nav = () => {
             data-twe-dropdown-alignment='end'
           >
             <Link
-              className='me-4 flex items-center text-neutral-600 dark:text-white'
+              className='hover:text-white me-4 flex items-center text-neutral-600 dark:text-white'
               href='/account'
               id='dropdownMenuButton1'
               role='button'
               data-twe-dropdown-toggle-ref
               aria-expanded='false'
             >
-
               Crate
             </Link>
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        blue: '#89A6FB',
+        white: '#FFF8F0'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
