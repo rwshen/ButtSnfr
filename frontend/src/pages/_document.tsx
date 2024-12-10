e2b270c8a96b3a2ba09947e7c7ee4883293223ae
rwshen <rshen91@gmail.com>
2024-12-10 09:45:24 -0700

    frontend basic work

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,12 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head } from "next/document";
+import Home from ".";
+import React from 'react';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
-        <Main />
-        <NextScript />
+        <Home />
       </body>
     </Html>
   );
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import localFont from "next/font/local";
+import React, { useState } from "react";
+import Main from "./main";
+import Register from "./register";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function Home() {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState<boolean>(false)
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+     {isUserLoggedIn ? <Main /> : <Register />}
+    </div>
+  );
+}
