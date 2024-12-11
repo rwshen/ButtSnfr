eecff9cb8e82ff7856623159d25a5433ca8f0f89
rwshen <rshen91@gmail.com>
2024-12-11 08:19:31 -0700

    post request for registering users

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -13,30 +13,49 @@ export const Register = () => {
   useEffect(() => {
   }, [])
 
-  const submitForm = useCallback((e) => {
-    e.preventDefault
-    fetch('http://localhost:3000').then(response => response.json()).then(data => console.log(data))
+  const submitForm = useCallback(async (e) => {
+    e.preventDefault()
+    await fetch('http://127.0.0.1:8080/api/register', {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        first_name: humanName,
+        dog_name: dogName,
+        address,
+        password,
+        email
+      })
+    }).then(response => {
+      if (!response.ok) {
+        console.log('error')
+      }
+      console.log(response)
+      return response
+    })
   }, [])
 
   return (
     <div className='flex mt-40'>
       <label className='flex flex-col'>
         <label className='pr-4'>Dog&apos;s Name:</label>
-        <input className='mb-4' name="dog_name" type='text' placeholder='Jojo' value={dogName} onChange={e => setDogName(e.currentTarget.value)} />
+        <input className='mb-4' name='dog_name' type='text' placeholder='Jojo' value={dogName} onChange={e => setDogName(e.currentTarget.value)} />
 
         <label className='pr-4'>Human&apos;s Name:</label>
-        <input className='mb-4' name="first_name" type='text' placeholder='Human of Jojo' value={humanName} onChange={e => setHumanName(e.currentTarget.value)} />
+        <input className='mb-4' name='first_name' type='text' placeholder='Human of Jojo' value={humanName} onChange={e => setHumanName(e.currentTarget.value)} />
 
         <label className='pr-4'>Jojo&apos;s Address:</label>
-        <input className='mb-4' type='text' name="address" placeholder='Crate address' value={address} onChange={e => setAddress(e.currentTarget.value)} />
+        <input className='mb-4' type='text' name='address' placeholder='Crate address' value={address} onChange={e => setAddress(e.currentTarget.value)} />
 
         <label className='pr-4'>Human&apos;s Email:</label>
-        <input className='mb-4' name="email" type='text' placeholder='Email' value={email} onChange={e => setEmail(e.currentTarget.value)} />
+        <input className='mb-4' name='email' type='text' placeholder='Email' value={email} onChange={e => setEmail(e.currentTarget.value)} />
 
         <label className='pr-4'>Create a password</label>
-        <input className='mb-2' name="password" type='text' placeholder='password' value={password} onChange={e => setPassword(e.currentTarget.value)} />
+        <input className='mb-2' name='password' type='text' placeholder='password' value={password} onChange={e => setPassword(e.currentTarget.value)} />
         <label className='pr-4'>Confirm password</label>
-        <input className='mb-20' name="confirm_password" type='text' placeholder='password' value={confirmPassword} onChange={e => setConfirmPassword(e.currentTarget.value)} />
+        <input className='mb-20' name='confirm_password' type='text' placeholder='password' value={confirmPassword} onChange={e => setConfirmPassword(e.currentTarget.value)} />
 
         <button className='hover:text-yellow' onClick={(e) => submitForm(e)}>Submit</button>
       </label>
