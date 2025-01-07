61039b984fe09cb1ab9b0e9a107c0138156b71fa
rwshen <rshen91@gmail.com>
2025-01-06 19:38:07 -0700

    switch to gatsby vs next

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    `./src/pages/**/*.{js,jsx,ts,tsx}`,
+    `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        blue: '#89A6FB',
-        white: '#FFF8F0',
-        yellow: '#BFAB25'
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
