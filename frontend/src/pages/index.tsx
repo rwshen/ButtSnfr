3e16e3f3435a4d286621ffefaf2ce5906da6fb0f
rwshen <rshen91@gmail.com>
2025-01-16 10:52:39 -0700

    set up logged in dashboard redirect

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { getOAuthToken } from "../api/usps_oauth";
-import { Register } from "../api/Register";
+import { Dashboard } from "../components/Dashboard";
 import { Nav } from "../components/Nav";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 export default async function Index() {
-  const [loggedIn, setLoggedIn] = useState<boolean>();
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [idToken, setIdToken] = useState<string | null>(localStorage.getItem('id_token'))
   // const {access_token} = await getOAuthToken();
   // console.log(access_token)
+  useEffect(() => {
+    if(idToken) {
+      setLoggedIn(true)
+    } setLoggedIn(false)
+  }, [])
+
   return (
     <>
       <header>
@@ -15,7 +22,7 @@ export default async function Index() {
       <div
         className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
       >
-        
+       {loggedIn && <Dashboard />} 
       </div>
       </>
   );
